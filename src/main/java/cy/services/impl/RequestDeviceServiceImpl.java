5261812790737fa4ab3d16b9b04324c76b138aac
Mai Minh Hiếu <hieumm.work@gmail.com>
2022-08-17 16:15:38 +0700

    hot fix

--- src/main/java/cy/services/impl/RequestDeviceServiceImpl.java
+++ src/main/java/cy/services/impl/RequestDeviceServiceImpl.java
@@ -7,6 +7,7 @@ import cy.entities.NotificationEntity;
 import cy.entities.RequestDeviceEntity;
 import cy.entities.UserEntity;
 import cy.models.RequestDeviceModel;
+import cy.models.RequestDeviceUpdateStatusModel;
 import cy.repositories.IHistoryRequestRepository;
 import cy.repositories.INotificationRepository;
 import cy.repositories.IRequestDeviceRepository;
@@ -73,8 +74,6 @@ public class RequestDeviceServiceImpl implements IRequestDeviceService {
         return this.iRequestDeviceRepository.findById(id).orElseThrow(()->new CustomHandleException(11));
     }
 
-    @Autowired
-    IHistoryRequestRepository historyRequestRepository;
     public void createHistory(RequestDeviceEntity requestDeviceEntity,int status){
         HistoryRequestEntity historyRequest=new HistoryRequestEntity();
         String pattern = "yyyy-MM-dd";
@@ -84,11 +83,9 @@ public class RequestDeviceServiceImpl implements IRequestDeviceService {
         historyRequest.setRequestDevice(requestDeviceEntity);
         historyRequestRepository.save(historyRequest);
     }
-    @Autowired
-    INotificationRepository notificationRepository;
+
     public void createNotification(RequestDeviceEntity requestDeviceEntity,Boolean isRead,String title,String content){
         NotificationEntity notificationEntity=new NotificationEntity();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         notificationEntity.setDateNoti(new java.util.Date());
         notificationEntity.setUserId(SecurityUtils.getCurrentUser().getUser());
         notificationEntity.setIsRead(isRead);
@@ -180,11 +177,11 @@ public class RequestDeviceServiceImpl implements IRequestDeviceService {
         return false;
     }
 
-    public RequestDeviceDto updateStatus(Long id, int caseSwitch, String reason) {
+    public RequestDeviceDto updateStatus(RequestDeviceUpdateStatusModel model) {
         UserEntity userEntity = SecurityUtils.getCurrentUser().getUser();
-        RequestDeviceEntity requestDeviceEntity = this.getById(id);
+        RequestDeviceEntity requestDeviceEntity = this.getById(model.getId());
         if(userEntity.getUserId()==requestDeviceEntity.getAssignTo().getUserId()){
-            switch (caseSwitch){
+            switch (model.getSwitchCase()){
                 case 1:
                     requestDeviceEntity.setStatus(1);
                     iRequestDeviceRepository.saveAndFlush(requestDeviceEntity);
@@ -194,7 +191,7 @@ public class RequestDeviceServiceImpl implements IRequestDeviceService {
                     break;
                 case 2:
                     requestDeviceEntity.setStatus(2);
-                    requestDeviceEntity.setReasonCancel(reason);
+                    requestDeviceEntity.setReasonCancel(model.getReasonCancel());
                     iRequestDeviceRepository.saveAndFlush(requestDeviceEntity);
                     createHistory(requestDeviceEntity,2);
                     createNotification(requestDeviceEntity,false,"Đã bị hủy bởi "+userEntity.getFullName(),"Yêu cầu cấp thiết bị");
@@ -206,7 +203,7 @@ public class RequestDeviceServiceImpl implements IRequestDeviceService {
        /* requestDeviceEntity.setStatus(1);
         iRequestDeviceRepository.saveAndFlush(requestDeviceEntity);
         createHistory(requestDeviceEntity,requestDeviceEntity.getStatus());*/
-        return RequestDeviceDto.entityToDto(iRequestDeviceRepository.findById(id).orElseThrow(() -> new CustomHandleException(11)));
+        return RequestDeviceDto.entityToDto(iRequestDeviceRepository.findById(model.getId()).orElseThrow(() -> new CustomHandleException(11)));
     }
     /*public RequestDeviceDto updateStatusCancle(Long id,String reason) {
         RequestDeviceEntity requestDeviceEntity = this.getById(id);
