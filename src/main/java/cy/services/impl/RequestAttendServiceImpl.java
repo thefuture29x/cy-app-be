0c006fe05e5f1a6007164a8e741b42d16cc5b6cc
Đinh Thế Vũ <thevu091193@gmail.com>
2022-08-17 11:39:23 +0700

    fix add request attend and notification

--- src/main/java/cy/services/impl/RequestAttendServiceImpl.java
+++ src/main/java/cy/services/impl/RequestAttendServiceImpl.java
@@ -86,7 +86,6 @@ public class RequestAttendServiceImpl implements IRequestAttendService {
     @Override
     public RequestAttendDto add(RequestAttendModel model) {
         RequestAttendEntity requestAttendEntity = this.modelToEntity(model);
-        RequestAttendEntity resultAttendEntity = this.requestAttendRepository.save(requestAttendEntity);
 
         // Today can't timekeeping for next day
         if(requestAttendEntity.getDateRequestAttend().after(new Date())){
@@ -94,16 +93,14 @@ public class RequestAttendServiceImpl implements IRequestAttendService {
         }
 
         // check request attend follow day and user not exist ???
-
         String day = new SimpleDateFormat("yyyy-MM-dd").format(model.getDateRequestAttend());
         Long userId = SecurityUtils.getCurrentUser().getUser().getUserId();
         List<RequestAttendEntity> requestAttendExist = this.requestAttendRepository.findByDayAndUser(day, userId);
+
         // cho tao moi neu khong co request attend nao hoac co request nhung da bi reject
         if(this.checkRequestAttendNotExist(day) || requestAttendExist.stream().anyMatch(x -> x.getStatus().equals(2))){
 
             RequestAttendEntity result = this.requestAttendRepository.save(requestAttendEntity);
-
-            RequestAttendEntity result2 = this.requestAttendRepository.save(requestAttendEntity);
 
             String title = "Request Attend";
             String content = "You have created a new request attend on " + model.getDateRequestAttend() + " from " + model.getTimeCheckIn() + " to " + model.getTimeCheckOut();
