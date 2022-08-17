05daf288260c2ed6af80442176f1e254e9529f84
nguyenquyen5120@gmail.com <nguyenquyen5120@gmail.com>
2022-08-17 10:54:33 +0700

    hot fix IRequestAttendRepository

--- src/main/java/cy/repositories/IRequestAttendRepository.java
+++ src/main/java/cy/repositories/IRequestAttendRepository.java
@@ -5,7 +5,6 @@ import cy.entities.RequestAttendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Date;
 import java.util.List;
 
 
