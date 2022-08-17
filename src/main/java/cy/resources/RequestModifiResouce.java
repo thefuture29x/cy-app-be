f8c215a18ce5320422f688b8a1bf44d3eb5d7c8c
SEABREEZE29X <teothiphong@gmail.com>
2022-08-17 08:59:00 +0700

    hot fix add request modifi

--- src/main/java/cy/resources/RequestModifiResouce.java
+++ src/main/java/cy/resources/RequestModifiResouce.java
@@ -7,17 +7,15 @@ import cy.models.RequestModifiModel;
 import cy.services.IResquestModifiService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping(FrontendConfiguration.PREFIX_API+"request_modifi/")
-public class RequesModifiResouce {
+public class RequestModifiResouce {
     @Autowired
     IResquestModifiService iResquestModifiService;
 
@@ -46,7 +44,7 @@ public class RequesModifiResouce {
     * @return: 
     *
     * */
-    @RolesAllowed({RoleEntity.MANAGER,RoleEntity.LEADER,RoleEntity.EMPLOYEE})
+    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER,RoleEntity.EMPLOYEE})
     @Operation(summary = "Add new request modifi")
     @PostMapping
     public ResponseDto addRequestModifi(RequestModifiModel model) throws IOException {
