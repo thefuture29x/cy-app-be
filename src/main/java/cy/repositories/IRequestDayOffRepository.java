44596560e39dddcabb3e100ef53e9637117dd868
Seabreeze <teothiphong@gmail.com>
2022-08-18 02:07:49 +0000

    Feature/crud rq modifi 7

--- src/main/java/cy/repositories/IRequestDayOffRepository.java
+++ src/main/java/cy/repositories/IRequestDayOffRepository.java
@@ -18,12 +18,12 @@ public interface IRequestDayOffRepository extends JpaRepository<RequestDayOffEnt
     Page<RequestDayOffEntity> findBypage(Pageable page);
 
     @Query(value = "SELECT * FROM `tbl_request_dayoff` \n " +
-            "WHERE user_id = ?1 \n " +
+            "WHERE assign_id = ?1 \n " +
             "and created_date between ?2 and ?3", nativeQuery = true)
     Page<RequestDayOffEntity> getAllRequestSendMe(Long id, String startTime, String endTime,Pageable pageable);
 
     @Query(value = "SELECT * FROM `tbl_request_dayoff` \n " +
-            "WHERE assign_id = ?1", nativeQuery = true)
+            "WHERE user_id = ?1", nativeQuery = true)
     Page<RequestDayOffEntity> getAllRequestCreateByMe(Long id, Pageable pageable);
 
 }
