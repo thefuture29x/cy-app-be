80aad0f9fb5404e96e0a180a44aa939c768e2a98
Seabreeze <teothiphong@gmail.com>
2022-08-24 01:56:50 +0000

    Feature/crud rq modifi 3

--- src/main/java/cy/repositories/IRequestOTRepository.java
+++ src/main/java/cy/repositories/IRequestOTRepository.java
@@ -14,10 +14,10 @@ import java.util.List;
 public interface IRequestOTRepository extends JpaRepository<RequestOTEntity, Long>, JpaSpecificationExecutor<RequestOTEntity> {
     @Query(value = "SELECT * FROM `tbl_request_ot` \n " +
             "WHERE assign_id = ?1 \n " +
-            "and created_date between ?2 and ?3", nativeQuery = true)
+            "and created_date between ?2 and ?3 ORDER BY updated_date DESC", nativeQuery = true)
     Page<RequestOTEntity> getAllRequestSendMe(Long id, String startTime, String endTime,Pageable pageable);
 
     @Query(value = "SELECT * FROM `tbl_request_ot` \n " +
-            "WHERE user_id = ?1", nativeQuery = true)
+            "WHERE user_id = ?1 ORDER BY updated_date DESC", nativeQuery = true)
     Page<RequestOTEntity> getAllRequestCreateByMe(Long id, Pageable pageable);
 }
