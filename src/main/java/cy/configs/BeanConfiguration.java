863c9ba18d326c3d28d2109e281bea9698e211f5
vuivehieu <vuivehieu@gmail.com>
2022-08-31 09:11:11 +0700

    Update BeanConfiguration.java

--- src/main/java/cy/configs/BeanConfiguration.java
+++ src/main/java/cy/configs/BeanConfiguration.java
@@ -46,8 +46,7 @@ public class BeanConfiguration {
                                 "http://localhost:8081",
                                 "http://localhost:8085",
                                 "http://localhost:19006",
-                                "http://localhost:19000",
-                                "http://3.35.67.53:3000")
+                                "http://localhost:19000")
                         .allowedOriginPatterns("*.*.*.*:*")
                         .allowCredentials(true)
                         .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS");
