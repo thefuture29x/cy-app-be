c7ff084b0b50be0a4d85c07a2e5971cd908c9b5e
dangnh-dev <haidang14727@gmail.com>
2022-09-15 15:54:27 +0700

    Update FileResource.java

--- src/main/java/cy/resources/FileResource.java
+++ src/main/java/cy/resources/FileResource.java
@@ -19,28 +19,28 @@ public class FileResource {
     @Autowired
     private IFileService fileService;
 
-    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER})
+    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER, RoleEntity.EMPLOYEE})
     @Operation(summary = "Upload new file")
     @PostMapping("upload")
     public ResponseDto uploadFile(@Valid @ModelAttribute FileModel fileModel) {
         return ResponseDto.of(fileService.add(fileModel));
     }
 
-    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER})
+    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER, RoleEntity.EMPLOYEE})
     @Operation(summary = "Update file")
     @PostMapping("updateFile")
     public ResponseDto updateFile(@Valid @ModelAttribute FileModel fileModel) {
         return ResponseDto.of(fileService.update(fileModel));
     }
 
-    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER})
+    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER, RoleEntity.EMPLOYEE})
     @Operation(summary = "Delete File")
     @DeleteMapping("delete")
     public ResponseDto deleteFile(Long id) {
         return ResponseDto.of(fileService.deleteById(id));
     }
 
-    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER})
+    @RolesAllowed({RoleEntity.ADMINISTRATOR,RoleEntity.ADMIN,RoleEntity.MANAGER,RoleEntity.LEADER, RoleEntity.EMPLOYEE})
     @Operation(summary = "Find file by id")
     @GetMapping("find-by-id/{id}")
     public ResponseDto findFileById(@PathVariable("id") Long id){
