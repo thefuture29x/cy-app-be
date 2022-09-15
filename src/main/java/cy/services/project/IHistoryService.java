3788af2967913f15348780daf31cd2ef1f50120d
Toever1503 <Haunv1503@gmail.com>
2022-09-15 15:38:52 +0700

    xoa history service

--- src/main/java/cy/services/project/IHistoryService.java
+++
@@ -1,7 +0,0 @@
-package cy.services.project;
-
-import cy.entities.project.HistoryEntity;
-import cy.services.IBaseService;
-
-public interface IHistoryService extends IBaseService<HistoryEntity, > {
-}
