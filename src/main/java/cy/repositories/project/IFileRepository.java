6e39b54dacfeb6c375a7f8c41d77009be01e70ff
SEABREEZE29X <teothiphong@gmail.com>
2022-09-16 16:14:23 +0700

    hot fix

--- src/main/java/cy/repositories/project/IFileRepository.java
+++ src/main/java/cy/repositories/project/IFileRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface IFileRepository extends JpaRepository<FileEntity, Long> {
+public interface IFileRepository extends JpaRepository<FileEntity, Long>, JpaSpecificationExecutor<FileEntity> {
     FileEntity findByFileNameAndObjectId(String fileName, Long objectId);
 }
