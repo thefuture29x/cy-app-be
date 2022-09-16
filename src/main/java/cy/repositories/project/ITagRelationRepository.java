80d732b634c6b69547e8ec26010f9eb5185b40cc
quangdz <coolquanghuu@gmail.com>
2022-09-16 08:19:57 +0700

    hot fix

--- src/main/java/cy/repositories/project/ITagRelationRepository.java
+++ src/main/java/cy/repositories/project/ITagRelationRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ITagRelationRepository extends JpaRepository<TagRelationEntity,Long> {
+
 }
