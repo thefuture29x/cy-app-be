fcad9329d4b93a5d29bbaf1277257374be04a74e
Đinh Thế Vũ <thevu091193@gmail.com>
2022-09-17 06:58:56 +0000

    CRUD Task

--- src/main/java/cy/resources/TaskResouce.java
+++ src/main/java/cy/resources/TaskResouce.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.transaction.Transactional;
 
 @RequestMapping(value = FrontendConfiguration.PREFIX_API + "task")
 @RestController
@@ -28,12 +29,29 @@ public class TaskResouce {
 
     @GetMapping(value = "/{id}")
     public ResponseDto getTaskById(@PathVariable("id") Long id) {
-        return ResponseDto.of(null);
+        return ResponseDto.of(this.taskService.findById(id));
     }
 
     @RolesAllowed({RoleEntity.ADMINISTRATOR, RoleEntity.ADMIN, RoleEntity.MANAGER, RoleEntity.EMPLOYEE, RoleEntity.LEADER})
     @PostMapping
+    @Transactional
     public ResponseDto addTask(TaskModel model) {
-        return ResponseDto.of(null);
+        model.setId(null);
+        return ResponseDto.of(this.taskService.add(model));
+    }
+
+    @RolesAllowed({RoleEntity.ADMINISTRATOR, RoleEntity.ADMIN, RoleEntity.MANAGER, RoleEntity.EMPLOYEE, RoleEntity.LEADER})
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseDto updateTask(TaskModel model, @PathVariable Long id) {
+        model.setId(id);
+        return ResponseDto.of(this.taskService.update(model));
+    }
+
+    @RolesAllowed({RoleEntity.ADMINISTRATOR, RoleEntity.ADMIN, RoleEntity.MANAGER, RoleEntity.EMPLOYEE, RoleEntity.LEADER})
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseDto deleteTask(@PathVariable Long id) {
+        return ResponseDto.of(this.taskService.deleteById(id));
     }
 }
