6de3f901ee58a6fde7ebe48c90eee04d9c402101
Seabreeze <teothiphong@gmail.com>
2023-02-14 02:15:52 +0000

    Feature/hot fix after restart prject

--- src/main/java/cy/repositories/project/IBugHistoryRepository.java
+++ src/main/java/cy/repositories/project/IBugHistoryRepository.java
@@ -3,8 +3,10 @@ package cy.repositories.project;
 import cy.dtos.project.BugHistoryDto;
 import cy.entities.project.BugHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface IBugHistoryRepository extends JpaRepository<BugHistoryEntity, Long> {
@@ -12,4 +14,10 @@ public interface IBugHistoryRepository extends JpaRepository<BugHistoryEntity, L
 
     @Query(value = "select * from tbl_bug_historys where bug_id = ?1", nativeQuery = true)
     List<BugHistoryDto> findByBugId(Long bugId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE tbl_bug_historys SET detail = ?1 WHERE id = ?2",nativeQuery = true)
+    void updateDetailHistoryBug(String detail,Long id);
+
 }
