08c96620c68f3be59e98df9162ad139a94521483
SEABREEZE29X <teothiphong@gmail.com>
2023-03-07 16:22:16 +0700

    hot fix get all task in project

--- src/main/java/cy/repositories/project/ITaskRepository.java
+++ src/main/java/cy/repositories/project/ITaskRepository.java
@@ -23,7 +23,12 @@ public interface ITaskRepository extends JpaRepository<TaskEntity, Long> {
     @Query(value = "select * from tbl_tasks where feature_id = ?1 and is_deleted = 0", nativeQuery = true)
     List<TaskEntity> findByFeatureId(Long id);
 
-    @Query(value = "SELECT tas.* FROM `tbl_projects` pro \n" + "JOIN `tbl_features` fea ON pro.id = fea.project_id\n" + "JOIN `tbl_tasks` tas ON fea.id = tas.feature_id\n" + "WHERE pro.id = ?1", nativeQuery = true)
+    @Query(value = "SELECT tas.* FROM `tbl_projects` pro \n" +
+            "JOIN `tbl_features` fea ON pro.id = fea.project_id\n" +
+            "JOIN `tbl_tasks` tas ON fea.id = tas.feature_id\n" +
+            "WHERE pro.id = ?1\n" +
+            "AND tas.is_deleted = 0 AND fea.is_deleted = 0 AND pro.is_deleted = 0\n" +
+            "AND tas.id not in (SELECT DISTINCT task_id FROM tbl_sub_tasks WHERE is_deleted = 0)\n", nativeQuery = true)
     Page<TaskEntity> findAllByProjectId(Long id, Pageable pageable);
 
     @Modifying
