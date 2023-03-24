9a23a61e192ef4c63d98b6cc9a55d2927b50a961
SEABREEZE29X <teothiphong@gmail.com>
2023-03-24 15:15:16 +0700

    hot fix

--- src/main/java/cy/repositories/project/IHistoryLogRepository.java
+++ src/main/java/cy/repositories/project/IHistoryLogRepository.java
@@ -11,16 +11,25 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface IHistoryLogRepository extends JpaRepository<HistoryEntity, Long>, JpaSpecificationExecutor<HistoryEntity> {
 
-    @Query(value = "SELECT DISTINCT * FROM `tbl_historys` \n" +
-            "WHERE category = 'BUG' AND object_id IN (\n" +
+    @Query(value = "SELECT DISTINCT * FROM tbl_historys WHERE category = 'BUG' \n" +
+            "AND object_id IN \n" +
+            "(\n" +
             "\tSELECT bg.id FROM `tbl_bugs` bg  \n" +
             "\tJOIN `tbl_sub_tasks` sts ON bg.sub_task_id = sts.id\n" +
             "\tJOIN `tbl_tasks` ts ON sts.task_id = ts.id\n" +
             "\tJOIN `tbl_features` ft ON ts.feature_id = ft.id \n" +
             "\tJOIN `tbl_projects` pr ON ft.project_id = pr.id \n" +
-            "\tWHERE pr.id = ?1\n" +
+            "\tWHERE pr.id = ?1 \n" +
             ")\n" +
-            "AND (content LIKE '%đã thêm mới bug%' OR content LIKE '%đã cập nhật bug%' OR content LIKE '%đã xóa bug%')\n",nativeQuery = true)
+            "OR object_id IN \n" +
+            "(\n" +
+            "\tSELECT bg.id FROM `tbl_bugs` bg  \n" +
+            "\tJOIN `tbl_tasks` ts ON bg.task_id = ts.id\n" +
+            "\tJOIN `tbl_features` ft ON ts.feature_id = ft.id \n" +
+            "\tJOIN `tbl_projects` pr ON ft.project_id = pr.id \n" +
+            "\tWHERE pr.id = ?1 \n" +
+            ")\n" +
+            "AND (content LIKE '%đã thêm mới bug%' OR content LIKE '%đã cập nhật bug%' OR content LIKE '%đã xóa bug%')",nativeQuery = true)
     Page<HistoryEntity> getAllHistoryOfBugInProject(Long idProject,Pageable pageable);
 
 
